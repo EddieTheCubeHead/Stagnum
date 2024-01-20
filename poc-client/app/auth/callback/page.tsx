b363094b84ba2fd378d52940bf2a6641667e2832
OttoAhlfors <otto.ahlfors@gmail.com>
2024-01-20 13:20:43 +0200

    send callback code to backend

---
+++ poc-client/app/auth/callback/page.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import axios from 'axios';
+import { useEffect } from 'react';
+
+export default function Page() {
+    const queryParams = new URLSearchParams(location.search);
+    const code = queryParams.get('code');
+
+    useEffect(() => {
+        if (code) {
+            handleTokenRequest(code);
+        }
+      }, [code]);
+  
+    const handleTokenRequest = (code: string) => {
+        console.log('Sending play request')
+
+        axios.get(`http://localhost:8000/auth/callback?code=${code}`, {})
+            .then(function (response) {
+                console.log(response);
+                //return response.data.token
+            }).catch((error) => {
+                console.log('Request failed', error);
+            })
+    }
+
+    return (
+        <main >
+        </main>
+    );
+}
