09216fc088818a02309ba22b26a1302538b22588
EddieTheCubeHead <eetu.asikainen1204@gmail.com>
2024-01-28 18:35:08 +0200

    10 add limit and offset for search routes

--- server/src/api/search/routes.py
+++ server/src/api/search/routes.py
@@ -14,20 +14,20 @@ async def search(query: str) -> GeneralSearchResult:
 
 
 @router.get("/tracks")
-async def search_tracks(query: str) -> PaginatedSearchResult[Track]:
+async def search_tracks(query: str, limit: int = 20, offset: int = 0) -> PaginatedSearchResult[Track]:
     pass
 
 
 @router.get("/albums")
-async def search_albums(query: str) -> PaginatedSearchResult[Album]:
+async def search_albums(query: str, limit: int = 20, offset: int = 0) -> PaginatedSearchResult[Album]:
     pass
 
 
 @router.get("/artists")
-async def search_artists(query: str) -> PaginatedSearchResult[Artist]:
+async def search_artists(query: str, limit: int = 20, offset: int = 0) -> PaginatedSearchResult[Artist]:
     pass
 
 
 @router.get("/playlists")
-async def search_playlists(query: str) -> PaginatedSearchResult[Playlist]:
+async def search_playlists(query: str, limit: int = 20, offset: int = 0) -> PaginatedSearchResult[Playlist]:
     pass
