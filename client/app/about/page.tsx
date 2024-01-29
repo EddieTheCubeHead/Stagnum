73b3f540615e9720abdcd4d1975433aeef0c2a89
ddevarsh7 <78840321+ddevarsh7@users.noreply.github.com>
2024-01-29 19:53:39 +0530

    File migration from poc-client to client

---
+++ client/app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function About() {
+  return (
+    <main className="min-h-screen bg-white text-black">
+      <h1>About Stagnum</h1>
+    </main>
+  );
+}
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,9 +1,31 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <h1 className={`mb-3 text-2xl font-semibold`}>Stagnum</h1>
-      </div>
+    <main className="relative min-h-screen bg-white text-black flex flex-col w-full h-full">
+      <aside className="fixed flex flex-col bg-neutral-200 h-full px-12 py-16 w-96">
+        <ul className="flex flex-col gap-4">
+          <li>Home</li>
+          <li>Search</li>
+          <li>Create Pool</li>
+          <li>Join Pool</li>
+          <li>My Pool</li>
+        </ul>
+      </aside>
+      <section className="relative flex flex-col h-full ml-96">
+        <button className="absolute right-12 h-24">My Profile</button>
+        <div className="fixed bg-black text-white bottom-0 left-0 w-full py-4 px-12 flex">
+          <div className="flex gap-4 w-96">
+            <a href="/about">About Stagnum</a>
+            <a
+              href="https://github.com/EddieTheCubeHead/Stagnum/discussions"
+              target="_blank"
+              className="text-[#1ED760]"
+            >
+              Contact Us
+            </a>
+          </div>
+          <p>Media Player</p>
+        </div>
+      </section>
     </main>
   );
 }
