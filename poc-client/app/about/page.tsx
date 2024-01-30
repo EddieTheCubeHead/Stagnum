ab391c04086fdb7bf756bb8f7b0619cef748e948
ddevarsh7 <ddevarsh7@gmail.com>
2024-01-30 06:25:30 +0200

    minor changes in poc-client
    had to make minor changes (due to a mistake)

--- poc-client/app/about/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function About() {
-  return (
-    <main className="min-h-screen bg-white text-black">
-      <h1>About Stagnum</h1>
-    </main>
-  );
-}
--- poc-client/app/contact/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Contact() {
-  return (
-    <main className="min-h-screen bg-white text-black">
-      <h1>Contact</h1>
-    </main>
-  );
-}
