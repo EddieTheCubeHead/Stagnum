f7c3332e97975f2fa929ac5f818f1ef6343a2109
OttoAhlfors <otto.ahlfors@gmail.com>
2024-02-04 21:53:23 +0200

    Searchfield appears when selected, MUI modifications use of placeholder theme

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,31 +1,55 @@
+'use client'
+
+import { Avatar, Box, Button, List, ListItem, ListItemButton, ListItemText, TextField, ThemeProvider, Typography } from "@mui/material";
+import Link from "next/link";
+import { useState } from "react";
+import theme from '@/services/stagnumTheme'
+
 export default function Home() {
+  const [showSearchBar, setShowSearchBar] = useState(false)
+
   return (
-    <main className="relative min-h-screen bg-white text-black flex flex-col w-full h-full">
-      <aside className="fixed flex flex-col bg-neutral-200 h-full px-12 py-16 w-96">
-        <ul className="flex flex-col gap-4">
-          <li>Home</li>
-          <li>Search</li>
-          <li>Create Pool</li>
-          <li>Join Pool</li>
-          <li>My Pool</li>
-        </ul>
-      </aside>
-      <section className="relative flex flex-col h-full ml-96">
-        <button className="absolute right-12 h-24">My Profile</button>
-        <div className="fixed bg-black text-white bottom-0 left-0 w-full py-4 px-12 flex">
-          <div className="flex gap-4 w-96">
-            <a href="/about">About Stagnum</a>
-            <a
-              href="https://github.com/EddieTheCubeHead/Stagnum/discussions"
-              target="_blank"
-              className="text-[#1ED760]"
-            >
-              Contact Us
-            </a>
-          </div>
-          <p>Media Player</p>
-        </div>
-      </section>
-    </main>
+    <main>
+      <ThemeProvider theme={theme}>
+        <Box sx={{ bgcolor: theme.palette.primary.light }} className="relative min-h-screen flex flex-col w-full h-full">
+
+          <Box sx={{ bgcolor: theme.palette.primary.main }} className="fixed flex flex-col h-full px-12 py-16 w-96">
+            <List>
+              <ListItemButton sx={{}} onClick={() => setShowSearchBar(!showSearchBar)}>
+                <ListItemText>Search</ListItemText>
+              </ListItemButton>
+            </List>
+          </Box>
+
+          <Box className="relative flex flex-col h-full ml-96">
+            <Avatar sx={{ margin: 2 }} className="absolute right-0 h-20 w-20" >My Profile</Avatar>
+            <Box sx={{}}>
+              {showSearchBar == true &&
+                <TextField
+                  sx={{ margin: 4, width: 500 }}
+                  id="standard-search"
+                  label="Search field"
+                  type="search"
+                  variant="standard"
+                />
+              }
+            </Box>
+            <Box sx={{ bgcolor: theme.palette.primary.dark }} className="fixed bottom-0 left-0 w-full py-4 px-12 flex">
+              <Box className="flex gap-4 w-96">
+                <Link href="/about">About Stagnum</Link>
+                <Link
+                  href="https://github.com/EddieTheCubeHead/Stagnum/discussions"
+                  target="_blank"
+                  className="text-[#1ED760]"
+                >
+                  Contact Us
+                </Link>
+              </Box>
+              <Typography>Media Player</Typography>
+            </Box>
+          </Box>
+        </Box>
+      </ThemeProvider>
+    </main >
   );
 }
