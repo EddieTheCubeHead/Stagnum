280f6a1324716937ef83ac5e5f8ac3659d61e815
EddieTheCubeHead <eetu.asikainen1204@gmail.com>
2024-02-17 23:00:30 +0200

    28 Fix broken alembic and accidentlly commited port changes

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -10,7 +10,7 @@ export default function Login() {
 
   const handleLoginRequest = () => {
     console.log('Sending login request')
-    const client_redirect_uri = 'http://localhost:3000'
+    const client_redirect_uri = 'http://localhost:80'
 
     axios
       .get('http://localhost:8080/auth/login', { params: { client_redirect_uri } })
