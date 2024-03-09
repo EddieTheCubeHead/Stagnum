335d35f9ae69e0c985731b98461d7d261e6369e9
OttoAhlfors <otto.ahlfors@gmail.com>
2024-03-09 12:29:08 +0200

    Color descriptions

--- client/utils/theme.ts
+++ client/utils/theme.ts
@@ -4,14 +4,14 @@ import { createTheme } from "@mui/material";
 const theme = createTheme({
   palette: {
     background: {
-      default: "#000", // Light grey
+      default: "#000", // Black
     },
     primary: {
       main: "#1976D2", // Blue
     },
     secondary: {
-      main: "#616161", // Dark grey
-      dark: "#212121", // Black
+      main: "#616161", // Gray
+      dark: "#212121", // Dark Grey
       light: "#D9D9D9", // Light grey
     },
     warning: {
