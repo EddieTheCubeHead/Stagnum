5aa0ee1e26be44af88fbdb5fcc44ed042d4675e8
EddieTheCubeHead <eetu.asikainen1204@gmail.com>
2024-03-22 15:25:16 +0200

    94 A few quick tests for share code

--- server/test/pool_features/shared_pool_features.py
+++ server/test/pool_features/shared_pool_features.py
@@ -2,5 +2,21 @@ def should_return_pool_code_from_share_route(existing_playback, test_client, val
     response = test_client.post("/pool/share", headers=valid_token_header)
 
     result = validate_response(response)
+    assert result["share_code"] is not None
     assert type(result["share_code"]) == str
+
+
+def should_have_only_uppercase_letters_and_digits_in_share_code(existing_playback, test_client, validate_response,
+                                                                valid_token_header):
+    response = test_client.post("/pool/share", headers=valid_token_header)
+
+    result = validate_response(response)
+    for char in result["share_code"]:
+        assert char.isupper() or char.isdigit()
+
+
+def should_have_eight_characters_in_share_code(existing_playback, test_client, validate_response, valid_token_header):
+    response = test_client.post("/pool/share", headers=valid_token_header)
+
+    result = validate_response(response)
     assert len(result["share_code"]) == 8
