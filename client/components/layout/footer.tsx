ef0d558d3746ebc862c027900bb1aca67efbb8f9
BadhonRahman01 <badhon.rahman184@gmail.com>
2024-03-27 03:09:04 +0200

    fix eslint issue in footer.tsx

--- client/components/layout/footer.tsx
+++ client/components/layout/footer.tsx
@@ -12,45 +12,61 @@ const Footer = (props: { token: string }) => {
         name: '90s Ambient Techno Mix',
         uri: 'spotify:playlist:37i9dQZF1EIfMxLinpTxdB',
         icon_link:
-          'https://seed-mix-image.spotifycdn.com/v6/img/desc/90s%20Ambient%20Techno/en/large',
-    };
+            'https://seed-mix-image.spotifycdn.com/v6/img/desc/90s%20Ambient%20Techno/en/large',
+    }
 
     return (
-        <Box sx={{
-            bgcolor: theme.palette.secondary.dark,
-            flexGrow: 1,
-            height: 60,
-            position: 'fixed',
-            bottom: 0,
-            left: 0,
-            right: 0,
-        }}
+        <Box
+            sx={{
+                bgcolor: theme.palette.secondary.dark,
+                flexGrow: 1,
+                height: 60,
+                position: 'fixed',
+                bottom: 0,
+                left: 0,
+                right: 0,
+            }}
+        >
             <Grid container>
-                <Grid item xs={9}
+                <Grid
+                    item
+                    xs={9}
                     display="flex"
                     justifyContent="center"
-                    alignItems="center" gap={2}>
+                    alignItems="center"
+                    gap={2}
+                >
                     {/*Image size fixed only for demo. Change when addressing first comment*/}
                     <img
                         src={playlist.icon_link}
-                        style={{width: 50, height: 50, padding: 0, margin: 0}}
+                        style={{ width: 50, height: 50, padding: 0, margin: 0 }}
                     />
-                    <Text text={playlist.name} fontWeight={'bold'} color={'white'}/>
-                    <SkipButton token={props.token}/>
+                    <Text
+                        text={playlist.name}
+                        fontWeight={'bold'}
+                        color={'white'}
+                    />
+                    <SkipButton token={props.token} />
                 </Grid>
-                <Grid 
-                    item xs={3} 
+                <Grid
+                    item
+                    xs={3}
                     display="flex"
                     justifyContent="center"
-                    alignItems="center">
-                    <Link sx={{
-                        color: theme.palette.primary.contrastText,
-                        paddingRight: 2,
-                    }}
-                    href="/about">About Stagnum</Link>
+                    alignItems="center"
+                >
                     <Link
                         sx={{
-                            color: theme.palette.secondary.main
+                            color: theme.palette.primary.contrastText,
+                            paddingRight: 2,
+                        }}
+                        href="/about"
+                    >
+                        About Stagnum
+                    </Link>
+                    <Link
+                        sx={{
+                            color: theme.palette.secondary.main,
                         }}
                         href="https://github.com/EddieTheCubeHead/Stagnum/discussions"
                         target="_blank"
