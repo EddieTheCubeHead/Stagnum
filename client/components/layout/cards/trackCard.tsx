9056163d7afdcd9f813386354f5f8191e74c1601
OttoAhlfors <otto.ahlfors@gmail.com>
2024-03-30 20:16:17 +0200

    Changed the card colors (did not want to make a new branch for this)

--- client/components/layout/cards/trackCard.tsx
+++ client/components/layout/cards/trackCard.tsx
@@ -20,7 +20,7 @@ export default function TrackCard(props: {
 
 
     return (
-        <Card sx={{ bgcolor: 'secondary.light', width: 1 }}>
+        <Card sx={{ bgcolor: 'secondary.main', width: 1 }}>
             <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
                 <Box sx={{ display: 'flex', alignItems: 'center' }}>
                     {props.track.album.link && (
