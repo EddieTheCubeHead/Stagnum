152bafa8c9b3fec2115792237d4a57683011d163
Tuomas Mustakallio <77295731+tuomasmustakallio@users.noreply.github.com>
2024-04-01 22:26:20 +0300

    Current pool state to pool manager

--- client/components/layout/poolManager.tsx
+++ client/components/layout/poolManager.tsx
@@ -1,12 +1,12 @@
 
-import { Box, Stack } from "@mui/material";
+import { Box, Stack, Grid, Avatar, AvatarGroup } from "@mui/material";
 import PoolCard from "./cards/poolCollectionCard";
 import PoolTrackCard from "./cards/poolCollectionCard";
 import PoolCollectionCard from "./cards/poolCollectionCard";
 import { useState } from "react";
 import axios from "axios";
 import DefaultButton from "../buttons/defaulButton";
-import { Header1 } from "../textComponents";
+import { Header2 } from "../textComponents";
 
 export default function ManagePool(props: {
     pool: Pool
@@ -17,20 +17,20 @@ export default function ManagePool(props: {
 
     const backend_uri = process.env.NEXT_PUBLIC_BACKEND_URI
     const token = props.token;
-    const [ res, setRes] = useState("")
     const handleShare = () => {
         axios
         .post(`${backend_uri}/pool/share`,{}, {
             headers: { token },
         })
         .then(function (response) {
-            console.log(response)
+            props.updatePool(response.data)
         })
         .catch((error) => {
             console.log("Request failed", error);
         });
     }
 
+    console.log(props.pool)
     return (
         <Box sx={{
             bgcolor: 'secondary.dark',
@@ -39,13 +39,38 @@ export default function ManagePool(props: {
             borderRadius: '12px',
             boxShadow: 2,
         }}>
-
             <Stack spacing={2} sx={{
                 width: 1,
                 margin: 1,
             }}>
-                <DefaultButton text="share" action={handleShare}/>
-                <Header1 text={res}/>
+                {props.expanded ?
+                <Box display={"flex"} justifyContent={"center"} alignItems={"center"} gap={1}>
+                    <Header2 text={props.pool.users.length > 0 ? `${props.pool.users[0].user.display_name}'s pool` : "Create or join a pool"} color={"secondary.light"} />
+                    {props.pool.share_code == null ? <DefaultButton text="share" action={handleShare}/> : <Header2 text={props.pool.share_code} color={"secondary.light"} />}
+                </Box> : 
+                <Grid container display={"flex"} justifyContent={"center"} alignItems={"center"}>
+                <Grid item xs={9}>
+                    <Header2 text={props.pool.users.length > 0 ? `${props.pool.users[0].user.display_name}'s pool` : "Create or join a pool"} color={"secondary.light"} />
+                </Grid>
+                <Grid item xs={1}>
+                    {props.pool.share_code == null ? <DefaultButton text="share" action={handleShare}/> : <Header2 text={props.pool.share_code} color={"secondary.light"} />}
+                </Grid>
+                <Grid item xs={2} display={"flex"} justifyContent={"center"} alignItems={"center"}>
+                    {props.pool.users.length > 3 ?
+                    <AvatarGroup total={props.pool.users.length}>
+                        {props.pool.users.map((user: PoolUser) => (
+                            <Avatar key={user.user.display_name} alt={user.user.display_name} src={user.user.icon_url} />
+                        ))}
+                    </AvatarGroup> :
+                    <>
+                        {props.pool.users.map((user: PoolUser) => (
+                            <Avatar key={user.user.display_name} alt={user.user.display_name} src={user.user.icon_url} />
+                        ))}
+                    </>
+                    }
+                </Grid>
+            </Grid> 
+                }
                 {props.pool?.users?.[0]?.tracks?.map((poolItem: any, key: number) => (
                     <PoolTrackCard poolItem={poolItem} key={key} token={props.token} updatePool={props.updatePool} />
                 ))}
