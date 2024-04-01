ab85162d87fa140977735326d716d12a5c161e33
Tuomas Mustakallio <77295731+tuomasmustakallio@users.noreply.github.com>
2024-04-01 21:10:43 +0300

    Input field for pool id and share and join buttons

--- client/components/layout/managePool.tsx
+++ client/components/layout/managePool.tsx
@@ -5,13 +5,31 @@ import Track from "@/types/trackTypes";
 import { Box, Container, Stack, Typography } from "@mui/material";
 import TrackCard from "./cards/trackCard";
 import PoolCard from "./cards/poolCard";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import DefaultButton from "../buttons/defaulButton";
+import axios from "axios";
+import { Header1 } from "../textComponents";
 
 export default function ManagePool(props: {
     pool: Pool
     token: string
     updatePool: (pool: Pool) => void
 }) {
+    const backend_uri = process.env.NEXT_PUBLIC_BACKEND_URI
+    const token = props.token;
+    const [ res, setRes] = useState("")
+    const handleShare = () => {
+        axios
+        .post(`${backend_uri}/pool/share`,{}, {
+            headers: { token },
+        })
+        .then(function (response) {
+            console.log(response)
+        })
+        .catch((error) => {
+            console.log("Request failed", error);
+        });
+    }
 
     return (
         <Box sx={{
@@ -25,6 +43,8 @@ export default function ManagePool(props: {
                 width: 1,
                 margin: 1,
             }}>
+                <DefaultButton text="share" action={handleShare}/>
+                <Header1 text={res}/>
                 {props.pool?.users?.[0]?.tracks?.map((poolItem: any, key) => (
                     <PoolCard poolItem={poolItem} key={key} token={props.token} updatePool={props.updatePool} />
                 ))}
