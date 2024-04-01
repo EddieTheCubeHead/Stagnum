d32aabe5b9b55c544de63075fa943660cbc242dc
BadhonRahman01 <badhon.rahman184@gmail.com>
2024-04-01 14:56:50 +0300

    Fixed some issue after merge

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,17 +1,6 @@
 'use client'
 
 import Footer from '@/components/layout/footer'
-import SideMenu from '@/components/layout/sideMenu'
-import { Box, CssBaseline, Grid, Stack } from '@mui/material'
-import axios from 'axios'
-import { useSearchParams, redirect } from 'next/navigation'
-import { Suspense, useEffect, useState } from 'react'
-import { ThemeProvider } from '@emotion/react'
-import theme from '../utils/theme'
-import MainHeaderCard from '@/components/layout/mainHeaderCard'
-import CreatePool from '@/components/layout/CreatePool'
-import Album from '@/types/albumTypes'
-import Footer from '@/components/layout/footer'
 import { Box, CssBaseline, Grid, Stack } from '@mui/material'
 import axios from 'axios'
 import { useSearchParams, redirect } from 'next/navigation'
@@ -37,9 +26,13 @@ export default function HomePage() {
 }
 
 function HomeContent() {
+    const [pool, setPool] = useState<Pool>({
+        users: [],
+        share_code: null,
+    })
     const [showSearchBar, setShowSearchBar] = useState(false)
     const [selectedCollections, setSellectedCollections] = useState<
-        Array<Album>
+        Array<Album | Track | Artist | Playlist>
     >([])
     const [token, setToken] = useState('')
     const queryParams = useSearchParams()
@@ -59,13 +52,11 @@ function HomeContent() {
     }, [])
 
     const handleTokenRequest = (code: string, state: string) => {
-        console.log('Sending play request')
-
         axios
-            .get(`${backend_uri}/auth/login/callback`, {
+            .get(`${process.env.NEXT_PUBLIC_BACKEND_URI}/auth/login/callback`, {
                 params: { state, code, client_redirect_uri },
             })
-            .then((response) => {
+            .then(function (response) {
                 setToken(response.data.access_token)
             })
             .catch((error) => {
@@ -73,14 +64,9 @@ function HomeContent() {
             })
     }
 
-    const handleAdd = (newAdd: Album) => {
-        setSellectedCollections((curCollections) => [...curCollections, newAdd])
-    }
-
-    const handleDelete = (itemToDelete: Album) => {
-        setSellectedCollections((curCollections) =>
-            curCollections.filter((collection) => collection !== itemToDelete),
-        )
+    // Function to add a new collection to a user
+    const updatePool = (pool: Pool) => {
+        setPool(pool)
     }
 
     return (
@@ -88,29 +74,28 @@ function HomeContent() {
             <CssBaseline />
             <Box
                 sx={{
-                    margin: 1,
+                    margin: 1.5,
+                    display: 'flex',
+                    height: 'calc(100vh - 80px)',
                 }}
             >
-                <Grid container gap={1}>
-                    <Grid item xs={4}>
-                        <Stack spacing={1}>
-                            <MainHeaderCard />
-                            <SideMenu
-                                setShowSearchBar={setShowSearchBar}
-                                showSearchBar={showSearchBar}
-                                token={token}
-                                handleAdd={handleAdd}
-                            />
-                        </Stack>
-                    </Grid>
-                    <Grid item xs={7.9}>
-                        <CreatePool
-                            token={token}
-                            selectedCollections={selectedCollections}
-                            handleDelete={handleDelete}
-                        />
-                    </Grid>
-                </Grid>
+                <Box
+                    sx={{
+                        flex: 1,
+                        padding: 1,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        minWidth: 370,
+                    }}
+                >
+                    <MainHeaderCard />
+                    <ManagePool
+                        pool={pool}
+                        token={token}
+                        updatePool={updatePool}
+                    />
+                </Box>
+                <Search token={token} updatePool={updatePool} />
             </Box>
             <Footer token={token} />
         </ThemeProvider>
