e52fc4596d766f24224c60e68e395b6375550e69
OttoAhlfors <otto.ahlfors@gmail.com>
2024-04-08 21:42:59 +0300

    format fix

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -1,12 +1,12 @@
 'use client'
 
-import axios from "axios";
-import { useRouter } from "next/navigation";
-import { Box, Grid, Link, Stack, Typography } from "@mui/material";
+import axios from 'axios'
+import { useRouter } from 'next/navigation'
+import { Box, Grid, Link, Stack, Typography } from '@mui/material'
 // import DefaultButton from "@/components/buttons/defaulButton";
-import Image from "next/image";
-import React from "react";
-import DefaultButton from "@/components/buttons/defaulButton";
+import Image from 'next/image'
+import React from 'react'
+import DefaultButton from '@/components/buttons/defaulButton'
 import { useState } from 'react'
 import AlertComponent from '@/components/alertComponent'
 
@@ -32,73 +32,77 @@ const LoginPage: React.FC = () => {
             })
     }
     const closeAlert = (): void => {
-      setAlert(false)
-  }
+        setAlert(false)
+    }
 
-  return (
-    <Grid
-      container
-      spacing={2}
-      bgcolor={"black"}
-      sx={{ minHeight: "100vh", width: "100%", margin: 0, padding: 0 }}
-      // justifyContent={"center"}
-      alignItems={"center"}
-    >
-      <Grid
-        item
-        xs={12}
-        sm={6}
-        justifyContent={"center"}
-        alignItems={"center"}
-        display={"flex"}
-        flexDirection={"column"}
-        gap={2}
-        zIndex={1}
-      >
-        <Stack
-          spacing={1}
-          direction={"row"}
-          alignItems={"end"}
-          ml={{ xs: 13, sm: 25, lg: 42 }}
-          mb={-3}
+    return (
+        <Grid
+            container
+            spacing={2}
+            bgcolor={'black'}
+            sx={{ minHeight: '100vh', width: '100%', margin: 0, padding: 0 }}
+            // justifyContent={"center"}
+            alignItems={'center'}
         >
-          <Typography fontSize={"1.25rem"} fontWeight={600} color={"white"}>
-            For
-          </Typography>
-          <Image
-            src={require("@/public/Spotify_Logo_RGB_Green.png")}
-            width={120}
-            height={40}
-            alt={"Home background"}
-            style={{ objectFit: "contain" }}
-          />
-        </Stack>
-        <Stack
-          width={{ xs: 280, sm: 400, lg: 500 }}
-          height={{ xs: 50, sm: 70, lg: 100 }}
-          // flex={1}
-          display={"flex"}
-          position={"relative"}
-        >
-          <Image
-            src={require("@/public/Stagnum_Logo.png")}
-            alt={"Home background"}
-            fill
-            style={{
-              objectFit: "contain",
-              flex: 1,
-            }}
-          />
-        </Stack>
-        <Typography
-          variant="caption"
-          fontSize={{ lg: "2rem", sm: "1.8rem", xs: "1.5rem" }}
-          color={"white"}
-          textAlign={"center"}
-        >
-          Simplified Collaborative Listening
-        </Typography>
-        
+            <Grid
+                item
+                xs={12}
+                sm={6}
+                justifyContent={'center'}
+                alignItems={'center'}
+                display={'flex'}
+                flexDirection={'column'}
+                gap={2}
+                zIndex={1}
+            >
+                <Stack
+                    spacing={1}
+                    direction={'row'}
+                    alignItems={'end'}
+                    ml={{ xs: 13, sm: 25, lg: 42 }}
+                    mb={-3}
+                >
+                    <Typography
+                        fontSize={'1.25rem'}
+                        fontWeight={600}
+                        color={'white'}
+                    >
+                        For
+                    </Typography>
+                    <Image
+                        src={require('@/public/Spotify_Logo_RGB_Green.png')}
+                        width={120}
+                        height={40}
+                        alt={'Home background'}
+                        style={{ objectFit: 'contain' }}
+                    />
+                </Stack>
+                <Stack
+                    width={{ xs: 280, sm: 400, lg: 500 }}
+                    height={{ xs: 50, sm: 70, lg: 100 }}
+                    // flex={1}
+                    display={'flex'}
+                    position={'relative'}
+                >
+                    <Image
+                        src={require('@/public/Stagnum_Logo.png')}
+                        alt={'Home background'}
+                        fill
+                        style={{
+                            objectFit: 'contain',
+                            flex: 1,
+                        }}
+                    />
+                </Stack>
+                <Typography
+                    variant="caption"
+                    fontSize={{ lg: '2rem', sm: '1.8rem', xs: '1.5rem' }}
+                    color={'white'}
+                    textAlign={'center'}
+                >
+                    Simplified Collaborative Listening
+                </Typography>
+
                 <Stack
                     spacing={2}
                     direction={'row'}
@@ -130,25 +134,31 @@ const LoginPage: React.FC = () => {
                         closeAlert={closeAlert}
                     />
                 )}
-                </Grid >
-     
-      <Grid
-        justifyContent={"center"}
-        alignItems={"center"}
-        display={"flex"}
-        flexDirection={"column"}
-        position={'relative'}
-        sx={{flex:1,height: '50vh', marginRight: '7%', marginLeft: '7%'}}
-        zIndex={1}>
-        <Image
-          src={require("@/public/newHomeBG.png")}
-          fill
-          alt={"Home background"}
-          style={{objectFit: 'contain',flex: 1, right: 150}}
-        />
-      </Grid>
-    </Grid>
-  );
+            </Grid>
+
+            <Grid
+                justifyContent={'center'}
+                alignItems={'center'}
+                display={'flex'}
+                flexDirection={'column'}
+                position={'relative'}
+                sx={{
+                    flex: 1,
+                    height: '50vh',
+                    marginRight: '7%',
+                    marginLeft: '7%',
+                }}
+                zIndex={1}
+            >
+                <Image
+                    src={require('@/public/newHomeBG.png')}
+                    fill
+                    alt={'Home background'}
+                    style={{ objectFit: 'contain', flex: 1, right: 150 }}
+                />
+            </Grid>
+        </Grid>
+    )
 }
 
 export default LoginPage
