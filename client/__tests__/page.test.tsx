a615879b06473a71414a92ef0c1a85bc82a27373
OttoAhlfors <otto.ahlfors@gmail.com>
2024-04-12 21:46:55 +0300

    Adding back about page now that build is successfull

--- client/__tests__/page.test.tsx
+++ client/__tests__/page.test.tsx
@@ -1,12 +1,12 @@
 /* eslint-disable testing-library/prefer-screen-queries */
 import { render } from '@testing-library/react'
 import React from 'react'
-import HomePage from '../app/page'
+import AboutPage from '../app/about/page'
 import '@testing-library/jest-dom'
 
 describe('Page', () => {
     it('Render starts with load', () => {
-        const { getByText } = render(<HomePage />)
+        const { getByText } = render(<AboutPage />)
         const specificText = getByText('About Stagnum')
         expect(specificText).toBeInTheDocument()
     })
---
+++ client/app/about/page.tsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from '@mui/material'
+
+const AboutPage: React.FC = () => {
+    return (
+        <Box>
+            <Typography>About page</Typography>
+        </Box>
+    )
+}
+
+export default AboutPage
