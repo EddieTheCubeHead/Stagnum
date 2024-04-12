431267bad2685061cd92c53acb69e27b4e02ac01
OttoAhlfors <otto.ahlfors@gmail.com>
2024-04-12 21:52:30 +0300

    deleted about page again since it causes Ci to fail for some reason

--- client/__tests__/page.test.tsx
+++ client/__tests__/page.test.tsx
@@ -1,12 +1,12 @@
 /* eslint-disable testing-library/prefer-screen-queries */
 import { render } from '@testing-library/react'
 import React from 'react'
-import AboutPage from '../app/about/page'
+import LoginPage from '../app/login/page'
 import '@testing-library/jest-dom'
 
 describe('Page', () => {
     it('Render starts with load', () => {
-        const { getByText } = render(<AboutPage />)
+        const { getByText } = render(<LoginPage />)
         const specificText = getByText('About Stagnum')
         expect(specificText).toBeInTheDocument()
     })
