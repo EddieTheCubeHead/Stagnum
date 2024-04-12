1d62d4eb9f36f77bd142090827895795e2e083e5
OttoAhlfors <otto.ahlfors@gmail.com>
2024-04-12 23:10:32 +0300

    fuck this shit

---
+++ client/__tests__/page.test.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable testing-library/prefer-screen-queries */
+import React from 'react'
+import { render } from '@testing-library/react'
+import NotAboutPage from '../app/notaboutpage/page'
+import '@testing-library/jest-dom'
+
+describe('NotAboutPage', () => {
+    it('renders correctly', () => {
+        const { getByText } = render(<NotAboutPage />)
+        const specificText = getByText('This is not about page')
+        expect(specificText).toBeInTheDocument()
+    })
+
+    // Add more test cases as needed
+})
---
+++ client/app/notaboutpage/page.tsx
@@ -0,0 +1,13 @@
+import { Box, Typography } from '@mui/material'
+
+const NotAboutPage: React.FC = () => {
+    return (
+        <Box>
+            <Typography variant="h1" color="initial">
+                This is not about page
+            </Typography>
+        </Box>
+    )
+}
+
+export default NotAboutPage
