4b2d1047788eb4ca6ad55f81896941ed0cc2aa18
KeManen <dev@kema.fi>
2024-04-13 21:20:57 +0300

    format

--- infra/terraform.tf
+++ infra/terraform.tf
@@ -1,20 +1,20 @@
 terraform {
-#    backend "s3" {
-#        bucket = var.app_name
-#        key = var.aws_access_key
-#        region = var.aws_region
-#    }
+  #    backend "s3" {
+  #        bucket = var.app_name
+  #        key = var.aws_access_key
+  #        region = var.aws_region
+  #    }
 
-    required_providers {
-      aws = {
-        source = "hashicorp/aws"
-        version = "~> 5.0"
-      }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
+  }
 }
 
 provider "aws" {
-    region = var.aws_region
-    access_key = var.aws_access_key
-    secret_key = var.aws_secret_key
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,61 +1,61 @@
 variable "aws_region" {
-  description   = "The AWS region where the resource is deployed"
-  type          = string
-  default       = "eu-north-1"
+  description = "The AWS region where the resource is deployed"
+  type        = string
+  default     = "eu-north-1"
 }
 
 variable "app_environment" {
-  description   = "The name of aws environment to be deployed"
-  type          = string
-  default       = "prod"
+  description = "The name of aws environment to be deployed"
+  type        = string
+  default     = "prod"
 }
 
 variable "app_name" {
-  description   = "The name of the application"
-  type          = string
-  default       = "stagnum"
+  description = "The name of the application"
+  type        = string
+  default     = "stagnum"
 }
 
 variable "aws_access_key" {
- description = "The aws access key"
- sensitive = true
- type = string
+  description = "The aws access key"
+  sensitive   = true
+  type        = string
 }
 
 variable "aws_secret_key" {
   description = "The aws secret key"
-  sensitive = true
-  type = string
+  sensitive   = true
+  type        = string
 }
 
 variable "spotify_client_id" {
   description = "The Spotify client id"
-  sensitive = true
-  type = string
+  sensitive   = true
+  type        = string
 }
 
 variable "spotify_client_secret" {
   description = "The Spotify client secret"
-  sensitive = true
-  type = string
+  sensitive   = true
+  type        = string
 }
 
 variable "postgres_user" {
   description = "The username for the database to use"
-  type = string
+  type        = string
 }
 
 variable "postgres_pass" {
   description = "The password for the database to use"
-  sensitive = true
-  type = string 
+  sensitive   = true
+  type        = string
 }
 
 variable "postgres_db" {
   description = "The database to use"
-  sensitive = false
-  type = string
-  
+  sensitive   = false
+  type        = string
+
 }
 
 variable "custom_weigth_scale" {
