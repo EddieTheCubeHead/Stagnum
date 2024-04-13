161f2d6fa5f0c0ae221dd62bb4a1395ee1f3a991
EddieTheCubeHead <eetu.asikainen1204@gmail.com>
2024-04-13 19:25:58 +0300

    96 Implement healthcheck route and basic tests

---
+++ server/src/api/health/dependencies.py
@@ -0,0 +1,38 @@
+import datetime
+from typing import Annotated
+
+from fastapi import Depends
+from sqlalchemy import text
+from sqlalchemy.exc import SQLAlchemyError
+
+from database.entities import PoolMember
+from api.common.dependencies import DateTimeWrapper, DatabaseConnection
+
+
+class RequestTimerRaw:
+
+    def __init__(self, datetime_wrapper: DateTimeWrapper):
+        self._start_time = datetime_wrapper.now()
+        self._datetime_wrapper = datetime_wrapper
+
+    def get_elapsed_time(self) -> datetime.timedelta:
+        return self._datetime_wrapper.now() - self._start_time
+
+
+RequestTimer = Annotated[RequestTimerRaw, Depends()]
+
+
+class HealthcheckDatabaseConnectionRaw:
+
+    def __init__(self, database_connection: DatabaseConnection):
+        self._database_connection = database_connection
+
+    @property
+    def is_healthy(self) -> bool:
+        try:
+            with self._database_connection.session() as session:
+                return session.scalar(text("SELECT 1")) == 1
+        except SQLAlchemyError:
+            return False
+
+HealthcheckDatabaseConnection = Annotated[HealthcheckDatabaseConnectionRaw, Depends()]
---
+++ server/src/api/health/routes.py
@@ -0,0 +1,26 @@
+from logging import getLogger
+
+from fastapi import APIRouter
+
+from api.common.dependencies import DateTimeWrapper
+from api.health.dependencies import RequestTimer, HealthcheckDatabaseConnection
+from api.health.models import HealthcheckResult, HEALTHY, HealthcheckResourceResult, UNHEALTHY
+
+_logger = getLogger("main.api.pool.routes")
+
+router = APIRouter(
+    prefix="/health",
+    tags=["health"]
+)
+
+@router.get("/")
+def healthcheck(request_timer: RequestTimer, healthcheck_database_connection: HealthcheckDatabaseConnection,
+                datetime_wrapper: DateTimeWrapper) \
+        -> HealthcheckResult:
+    database_timer_start = datetime_wrapper.now()
+    database_status = HEALTHY if healthcheck_database_connection.is_healthy else UNHEALTHY
+    database_timer = datetime_wrapper.now() - database_timer_start
+    database_result = HealthcheckResourceResult(resource="database", status=database_status,
+                                                time_elapsed=database_timer)
+    return HealthcheckResult(status=database_result.status, time_elapsed=request_timer.get_elapsed_time(),
+                             resources=[database_result])
