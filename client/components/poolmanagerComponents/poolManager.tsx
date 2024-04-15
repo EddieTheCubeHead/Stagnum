5f830c2114f8e2ada7d66723b8ce9a768859e871
ddevarsh7 <ddevarsh7@gmail.com>
2024-04-15 19:34:08 +0300

    a minor change

--- client/components/poolmanagerComponents/poolManager.tsx
+++ client/components/poolmanagerComponents/poolManager.tsx
@@ -52,7 +52,7 @@ const PoolManager: React.FC<PoolManagerProps> = ({
                 display: 'flex',
                 overflow: 'auto',
                 borderRadius: '12px',
-                boxShadow: 2,
+                boxShadow: '3px 3px 3px',
                 px:1,
             }}
         >
