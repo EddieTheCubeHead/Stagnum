70786d2cb93c039571cafb74937a81ccb162becd
EddieTheCubeHead <eetu.asikainen1204@gmail.com>
2024-05-02 13:24:14 +0300

    #118 typing for search conftest file

--- server/test/search_features/conftest.py
+++ server/test/search_features/conftest.py
@@ -1,11 +1,20 @@
 import random
+from typing import Callable, Any, Protocol
 
 import pytest
 
 
+class _ValidatePaginatedResultLengthProtocol(Protocol):
+    def __call__(self, result: dict[str, Any], length: int = ..., offset: int = ...) -> None:
+        ...
+
+
+validate_paginated_result_length_callable = _ValidatePaginatedResultLengthProtocol
+
+
 @pytest.fixture
-def validate_paginated_result_length():
-    def wrapper(result: dict, length=20, offset=0):
+def validate_paginated_result_length() -> validate_paginated_result_length_callable:
+    def wrapper(result: dict, length: int = 20, offset: int = 0) -> None:
         assert len(result["results"]) == length
         assert result["limit"] == length
         assert result["offset"] == offset
@@ -13,9 +22,12 @@ def validate_paginated_result_length():
     return wrapper
 
 
+create_paginated_search_result_callable = Callable[[str, int, list[dict[str, Any]]], dict[str, Any]]
+
+
 @pytest.fixture
-def create_paginated_search_result():
-    def wrapper(query: str, limit: int, items: list[dict]):
+def create_paginated_search_result() -> create_paginated_search_result_callable:
+    def wrapper(query: str, limit: int, items: list[dict[str, Any]]) -> dict[str, Any]:
         return {
             "href": f"https://api.spotify.com/v1/search?query={query}&type=track&offset=0&limit=20",
             "limit": limit,
