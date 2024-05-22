4364280ac0607bca1ca4a84d6af4ebc66d4f5c4c
EddieTheCubeHead <eetu.asikainen1204@gmail.com>
2024-05-22 20:05:10 +0300

    #319 setup tests for ui rewrite

--- clientRewrite/vite.config.ts
+++ clientRewrite/vite.config.ts
@@ -1,7 +1,12 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
 
-// https://vitejs.dev/config/
+/** @type {import('vite').UserConfig} */
 export default defineConfig({
     plugins: [react()],
+    test: {
+        root: "./test",
+        include: ["**/*_features.ts"],
+    },
 })
