3f2d79b1c38472231577c4ef6a41353063c80849
EddieTheCubeHead <eetu.asikainen1204@gmail.com>
2024-06-29 00:34:23 +0300

    #357 Ensure tests never touch websocket code
    Wonky I know

--- clientRewrite/setup-vitest.ts
+++ clientRewrite/setup-vitest.ts
@@ -1,9 +1,20 @@
-import { afterEach } from "vitest"
+import { beforeEach, afterEach, vi } from "vitest"
 import { cleanup } from "@testing-library/react"
 import { useTokenStore } from "./src/common/stores/tokenStore"
 import { Theme, useThemeStore } from "./src/common/stores/themeStore"
 import { useAlertStore } from "./src/alertSystem/alertStore"
 import { usePoolStore } from "./src/common/stores/poolStore"
+import { useStartWebSocket } from "./src/common/hooks/useStartWebSocket"
+
+beforeEach(() => {
+    vi.mock("./src/common/hooks/useStartWebSocket", () => {
+        return {
+            useStartWebSocket: () => {
+                return () => {}
+            },
+        }
+    })
+})
 
 afterEach(() => {
     cleanup()
