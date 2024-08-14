53ec0436731c9941f70b240c07767de47700bd5f
EddieTheCubeHead <eetu.asikainen1204@gmail.com>
2024-08-14 23:11:37 +0300

    #409 run lint and format for client

--- clientRewrite/src/common/hooks/usePostDemoteTrack.ts
+++ clientRewrite/src/common/hooks/usePostDemoteTrack.ts
@@ -1,12 +1,12 @@
-import {PoolMember} from "../models/PoolMember.ts";
-import {useAlertStore} from "../../alertSystem/alertStore.ts";
-import {useApiPost} from "../../api/methods.ts";
-import {Pool} from "../models/Pool.ts";
-import {AlertType} from "../../alertSystem/Alert.ts";
+import { PoolMember } from "../models/PoolMember.ts"
+import { useAlertStore } from "../../alertSystem/alertStore.ts"
+import { useApiPost } from "../../api/methods.ts"
+import { Pool } from "../models/Pool.ts"
+import { AlertType } from "../../alertSystem/Alert.ts"
 
 export const usePostDemoteTrack = (track: PoolMember) => {
     const { addAlert } = useAlertStore()
-    const demote = useApiPost<Pool>('/pool/demote')
+    const demote = useApiPost<Pool>("/pool/demote")
 
     return () => {
         demote({}).then(() => {
--- clientRewrite/src/pool/components/PromotePoolTrackIconButton.tsx
+++ clientRewrite/src/pool/components/PromotePoolTrackIconButton.tsx
@@ -5,8 +5,8 @@ import { usePostPromoteTrack } from "../../common/hooks/usePostPromoteTrack.ts"
 import { usePoolPromotedSongs } from "../../common/hooks/usePoolPromotedSongs.ts"
 import { Avatar } from "../../common/components/avatar/Avatar.tsx"
 import { Size } from "../../common/constants/size.ts"
-import {usePostDemoteTrack} from "../../common/hooks/usePostDemoteTrack.ts";
-import {useMeQuery} from "../../common/hooks/useMeQuery.ts";
+import { usePostDemoteTrack } from "../../common/hooks/usePostDemoteTrack.ts"
+import { useMeQuery } from "../../common/hooks/useMeQuery.ts"
 
 interface PromotePoolTrackIconButtonProps {
     poolMember: PoolMember
@@ -17,7 +17,7 @@ export const PromotePoolTrackIconButton = ({ poolMember }: PromotePoolTrackIconB
     const demotePoolMembers = usePostDemoteTrack(poolMember)
     const promotedPoolMembers = usePoolPromotedSongs()
     const promotedByUser = promotedPoolMembers.get(poolMember.id)
-    const { user} = useMeQuery()
+    const { user } = useMeQuery()
     const isPromotedBySelf = !!promotedByUser && promotedByUser?.spotify_id == user?.spotify_id
     const callback = isPromotedBySelf ? demotePoolMembers : promotePoolMember
     return (
