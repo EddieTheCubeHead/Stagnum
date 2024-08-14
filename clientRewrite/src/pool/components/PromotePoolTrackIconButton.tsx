da79b76bda6ec5000f28185c9bd17eeed84eea15
EddieTheCubeHead <eetu.asikainen1204@gmail.com>
2024-08-14 23:06:28 +0300

    #409 Demote song when clicking a self-promoted song in pool view

--- clientRewrite/src/pool/components/PromotePoolTrackIconButton.tsx
+++ clientRewrite/src/pool/components/PromotePoolTrackIconButton.tsx
@@ -5,6 +5,8 @@ import { usePostPromoteTrack } from "../../common/hooks/usePostPromoteTrack.ts"
 import { usePoolPromotedSongs } from "../../common/hooks/usePoolPromotedSongs.ts"
 import { Avatar } from "../../common/components/avatar/Avatar.tsx"
 import { Size } from "../../common/constants/size.ts"
+import {usePostDemoteTrack} from "../../common/hooks/usePostDemoteTrack.ts";
+import {useMeQuery} from "../../common/hooks/useMeQuery.ts";
 
 interface PromotePoolTrackIconButtonProps {
     poolMember: PoolMember
@@ -12,13 +14,17 @@ interface PromotePoolTrackIconButtonProps {
 
 export const PromotePoolTrackIconButton = ({ poolMember }: PromotePoolTrackIconButtonProps) => {
     const promotePoolMember = usePostPromoteTrack(poolMember)
+    const demotePoolMembers = usePostDemoteTrack(poolMember)
     const promotedPoolMembers = usePoolPromotedSongs()
     const promotedByUser = promotedPoolMembers.get(poolMember.id)
+    const { user} = useMeQuery()
+    const isPromotedBySelf = !!promotedByUser && promotedByUser?.spotify_id == user?.spotify_id
+    const callback = isPromotedBySelf ? demotePoolMembers : promotePoolMember
     return (
         <div className="relative">
             {poolMember.spotify_resource_uri.startsWith("spotify:track:") && (
                 <>
-                    <IconButton svg={<StarIconSvg />} onClick={promotePoolMember} toggled={!!promotedByUser} />
+                    <IconButton svg={<StarIconSvg />} onClick={callback} toggled={!!promotedByUser} />
                     {promotedByUser && (
                         <span className="absolute bottom-0 right-0 z-10">
                             <Avatar avatarUser={promotedByUser} size={Size.xs} />
