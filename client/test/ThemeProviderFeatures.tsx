c53964fd4e58f042e9d5ec08c89ccc92bd15784e
EddieTheCubeHead <eetu.asikainen1204@gmail.com>
2025-06-26 19:35:58 +0300

    #373 Missed one

--- client/test/ThemeProviderFeatures.tsx
+++ client/test/ThemeProviderFeatures.tsx
@@ -1,11 +1,12 @@
-import { describe, expect, it, vi } from "vitest"
-import { render, screen } from "@testing-library/react"
+import { describe, expect, it } from "vitest"
+import { screen } from "@testing-library/react"
 import { ThemeProvider } from "../src/ThemeProvider"
 import { Theme, useThemeStore } from "../src/common/stores/themeStore"
+import testComponent from "./utils/testComponent.tsx"
 
 describe("ThemeProvider", () => {
     it("Should include theme in classname", () => {
-        render(<ThemeProvider>component under test</ThemeProvider>)
+        testComponent(<ThemeProvider>component under test</ThemeProvider>)
 
         expect(screen.getByText("component under test").className).toContain(Theme.Dark)
     })
@@ -13,7 +14,7 @@ describe("ThemeProvider", () => {
     it.each([Theme.Dark, Theme.Light])("Should get theme from theme store", (theme) => {
         useThemeStore.setState({ theme: theme })
 
-        render(<ThemeProvider>component under test</ThemeProvider>)
+        testComponent(<ThemeProvider>component under test</ThemeProvider>)
 
         expect(screen.getByText("component under test").className).toContain(theme)
     })
