23eee03ca3ff205b0e5f92ae8dbb9ba3e8fb9b7e
EddieTheCubeHead <eetu.asikainen1204@gmail.com>
2025-06-30 21:45:31 +0300

    #424 More hacky typechecker fixes

--- client/src/api/fetchRedirectUri.ts
+++ client/src/api/fetchRedirectUri.ts
@@ -1,7 +1,8 @@
 import { apiGet } from "./methods.ts"
+import { RedirectResponse } from "../login/models/RedirectResponse.ts"
 
 export const fetchRedirectUri = async () => {
     const client_redirect_uri = import.meta.env.VITE_FRONTEND_URL
-    const response = await apiGet("/auth/login", undefined, { client_redirect_uri })
+    const response = await apiGet<RedirectResponse>("/auth/login", undefined, { client_redirect_uri })
     return response.data
 }
--- client/src/api/fetchToken.ts
+++ client/src/api/fetchToken.ts
@@ -1,5 +1,5 @@
 import { apiGet } from "./methods.ts"
-import { SpotifyToken } from "../common/models/SpotifyToken.ts"
+import { SpotifyToken } from "../login/models/SpotifyToken.ts"
 
 export const fetchToken = async (code: string, state: string) => {
     const client_redirect_uri = import.meta.env.VITE_FRONTEND_URL
